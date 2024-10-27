f7fa1eace8606e6742cca2598f6f4b10bd75b597
UniquePtr114514 <3485621569@qq.com>
2024-10-27 20:19:14 +0800

    fix random

--- src/api/utils/RandomUtils.cpp
+++ src/api/utils/RandomUtils.cpp
@@ -4,7 +4,7 @@
 namespace utils {
 
 template <typename T>
-T randomNumber(T min = std::numeric_limits<T>::min(), T max = std::numeric_limits<T>::max()) {
+T randomInteger(T min = std::numeric_limits<T>::min(), T max = std::numeric_limits<T>::max()) {
     std::random_device               rd;
     std::mt19937                     gen(rd());
     std::uniform_int_distribution<T> distr(min, max);
@@ -12,13 +12,22 @@ T randomNumber(T min = std::numeric_limits<T>::min(), T max = std::numeric_limit
     return random_num;
 }
 
-int Random::nextInt() { return randomNumber<int>(); }
+template <typename T>
+T randomNumber(T min = std::numeric_limits<T>::min(), T max = std::numeric_limits<T>::max()) {
+    std::random_device                rd;
+    std::mt19937                      gen(rd());
+    std::uniform_real_distribution<T> distr(min, max);
+    T                                 random_num = distr(gen);
+    return random_num;
+}
 
-int Random::nextInt(int min, int max) { return randomNumber<int>(min, max); }
+int Random::nextInt() { return randomInteger<int>(); }
 
-int64_t Random::nextLong() { return randomNumber<int64_t>(); }
+int Random::nextInt(int min, int max) { return randomInteger<int>(min, max); }
 
-int64_t Random::nextLong(int64_t min, int64_t max) { return randomNumber<int64_t>(min, max); }
+int64_t Random::nextLong() { return randomInteger<int64_t>(); }
+
+int64_t Random::nextLong(int64_t min, int64_t max) { return randomInteger<int64_t>(min, max); }
 
 float Random::nextFloat() { return randomNumber<float>(); }
 
