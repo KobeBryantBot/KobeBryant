3857dee9433c6fa26ebaeed44091f96ee2460b6a
UniquePtr114514 <186297309+UniquePtr114514@users.noreply.github.com>
2024-11-06 09:34:10 +0800

    improve script

--- pack_sdk.py
+++ pack_sdk.py
@@ -13,7 +13,10 @@ os.makedirs(sdk_folder_path)
 include_folder_path = os.path.join(current_dir, "include")
 lib_folder_path = os.path.join(current_dir, "lib")
 
-
 shutil.copytree(include_folder_path, os.path.join(sdk_folder_path, "include"))
 shutil.copytree(lib_folder_path, os.path.join(sdk_folder_path, "lib"))
-shutil.copy("./bin/MinSizeRel/KobeBryant.lib", "./sdk/lib/KobeBryant.lib")
+
+if os.path.exists("./build/MinSizeRel/KobeBryant.lib"):
+    shutil.copy("./build/MinSizeRel/KobeBryant.lib", "./sdk/lib/KobeBryant.lib")
+elif os.path.exists("./build/Release/KobeBryant.lib"):
+    shutil.copy("./build/Release/KobeBryant.lib", "./sdk/lib/KobeBryant.lib")
