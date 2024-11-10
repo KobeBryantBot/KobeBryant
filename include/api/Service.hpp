f2bfb125bc30dc9596ef001d893392c12806cae2
Fluorine <186297309+DivanadiumPentaoxide@users.noreply.github.com>
2024-11-10 10:08:51 +0800

    update servive

--- include/api/Service.hpp
+++ include/api/Service.hpp
@@ -9,12 +9,13 @@ public:
     using Func = Ret (*)(Args...);
 
 protected:
-    KobeBryant_NDAPI static bool    exportFuncPtr(std::string const& funcName, HMODULE hModule, FARPROC func);
-    KobeBryant_NDAPI static FARPROC importFuncPtr(std::string const& pluginName, std::string const& funcName);
-    KobeBryant_NDAPI static bool    removeFuncPtr(HMODULE hModule, std::string const& funcName);
+    KobeBryant_NDAPI static bool exportFuncPtr(std::string const& funcName, HMODULE hModule, FARPROC func);
+    KobeBryant_NDAPI static bool removeFuncPtr(HMODULE hModule, std::string const& funcName);
 
 public:
     KobeBryant_NDAPI static bool hasFunc(std::string const& pluginName, std::string const& funcName);
+
+    KobeBryant_NDAPI static FARPROC importFuncPtr(std::string const& pluginName, std::string const& funcName);
 
     template <class Fn>
     static bool exportFunc(std::string const& funcName, Fn&& func) {
