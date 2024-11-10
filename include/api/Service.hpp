77238f85f1f485479ab3fb9a2d3c209864bb5008
Fluorine <186297309+DivanadiumPentaoxide@users.noreply.github.com>
2024-11-10 19:46:23 +0800

    fix service

--- include/api/Service.hpp
+++ include/api/Service.hpp
@@ -21,9 +21,16 @@ public:
     static inline bool exportFunc(std::string const& funcName, std::function<Ret(Args...)> const& func) {
         auto pluginName = utils::getCurrentPluginName();
         auto anyFunc    = [func](std::vector<std::any> const& args) -> std::any {
-            auto args_tuple = any_vector_to_tuple<Args...>(args);
-            auto result     = func(std::get<Args>(args_tuple)...);
-            return result;
+            if (sizeof...(Args) != args.size()) {
+                throw std::runtime_error("Wrong number of arguments.");
+            }
+            auto args_tuple = make_args<Args...>(args, std::index_sequence_for<Args...>{});
+            if constexpr (std::is_void<Ret>::value) {
+                std::apply(func, args_tuple);
+                return std::any();
+            } else {
+                return std::any(std::apply(func, args_tuple));
+            }
         };
         return exportAnyFunc(pluginName, funcName, anyFunc);
     }
@@ -31,11 +38,18 @@ public:
     template <typename Ret, typename... Args>
     static inline std::function<Ret(Args...)> importFunc(std::string const& pluginName, std::string const& funcName) {
         auto func = importAnyFunc(pluginName, funcName);
-        return [func](Args... args) -> Ret {
-            std::vector<std::any> anyArgs = {std::any(args)...};
-            std::any              result  = func(anyArgs);
-            return std::any_cast<Ret>(result);
-        };
+        if constexpr (std::is_void<Ret>::value) {
+            return [func](Args... args) {
+                std::vector<std::any> anyArgs = {std::any(args)...};
+                func(anyArgs);
+            };
+        } else {
+            return [func](Args... args) -> Ret {
+                std::vector<std::any> anyArgs = {std::any(args)...};
+                std::any              result  = func(anyArgs);
+                return std::any_cast<Ret>(result);
+            };
+        }
     }
 
     KobeBryant_NDAPI static bool hasFunc(std::string const& pluginName, std::string const& funcName);
@@ -47,23 +61,13 @@ public:
 
 protected:
     template <typename... Args, std::size_t... Is>
-    static inline std::tuple<Args...>
-    any_vector_to_tuple_impl(std::vector<std::any> const& vec, std::index_sequence<Is...>) {
+    static inline std::tuple<Args...> make_args(std::vector<std::any> const& vec, std::index_sequence<Is...>) {
         return std::make_tuple(std::any_cast<Args>(vec[Is])...);
     }
 
-    template <typename... Args>
-    static inline std::tuple<Args...> any_vector_to_tuple(std::vector<std::any> const& vec) {
-        if (sizeof...(Args) != vec.size()) {
-            throw std::runtime_error("Vector size does not match the number of arguments.");
-        }
-        return any_vector_to_tuple_impl<Args...>(vec, std::index_sequence_for<Args...>{});
-    }
+    KobeBryant_NDAPI static bool exportAnyFunc(std::string const&, std::string const&, Service::AnyFunc const&);
 
-    KobeBryant_NDAPI static bool
-    exportAnyFunc(std::string const& pluginName, std::string const& funcName, Service::AnyFunc const& func);
+    KobeBryant_NDAPI static Service::AnyFunc importAnyFunc(std::string const&, std::string const&);
 
-    KobeBryant_NDAPI static Service::AnyFunc importAnyFunc(std::string const& pluginName, std::string const& funcName);
-
-    KobeBryant_NDAPI static bool removeFunc(std::string const& pluginName, std::string const& funcName);
+    KobeBryant_NDAPI static bool removeFunc(std::string const&, std::string const&);
 };
